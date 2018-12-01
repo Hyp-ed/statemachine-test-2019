0c22188455a54076ef737c614454975dc7022050
Calum McM <calummcmeekin@me.com>
2018-12-01 20:19:53 +0000

    Rudimentary test made for Project 1

--- playground.cpp
+++ playground.cpp
@@ -5,7 +5,54 @@
 #include <thread>
 #include <chrono>
 
+
+
+//Set up simulator for variables
+bool kCriticalFailure = true;
+bool comms_data_ = false;
+bool nav_data_ = false;
+bool motor_data_ = true;
+bool batteries_data_ = true;
+//Continue with program
+
+
+bool checkCriticalFailure() {
+    bool criticalFailureFound = false;
+    // check if any of the module has failed (except sensors)
+    if (comms_data_/*.module_status*/ == /*data::ModuleStatus::*/kCriticalFailure) {
+        std::cout << /*log_.ERR("STATE"*/ "Critical failure caused by communications " << std::endl;
+        criticalFailureFound = true;
+        //return true
+    }
+    if (nav_data_/*.module_status*/ == /*data::ModuleStatus::*/kCriticalFailure) {
+        std::cout << /*log_.ERR("STATE",*/ "Critical failure caused by navigation " << std::endl;
+        criticalFailureFound = true;
+        //return true;
+    }
+    if (motor_data_/*.module_status*/ == /*data::ModuleStatus::*/kCriticalFailure) {
+        std::cout << /*log_.ERR("STATE",*/ "Critical failure caused by motors " << std::endl;
+        criticalFailureFound = true;
+        //return true;
+    }
+    if (batteries_data_/*.module_status*/ == /*data::ModuleStatus::*/kCriticalFailure) {
+        std::cout << /*log_.ERR("STATE", */"Critical failure caused by batteries " << std::endl;
+        criticalFailureFound = true;
+        //return true;
+    }
+    if (criticalFailureFound) {
+        /*hypedMachine.handleEvent(kCriticalFailure);*/
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main(){
-    std::cout << "Hello World" << std::endl;
+
+    if(checkCriticalFailure()){
+        std::cout << "\033[1;32m    TEST PASSED\033[0m " << std::endl;
+    }
+
+
     return 0;
 }
